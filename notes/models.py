6f17ef298b4bca931d43cd8d3b171edf423825e0
Oleg Panichev <o_pan@mail.ru>
2021-04-17 12:25:09 +0300

    Made date callable

--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from datetime import datetime, timedelta
 
+def note_time():
+    return datetime.now()+timedelta(days=1)
+
 # Create your models here.
 class Authors(models.Model):
     name = models.CharField(max_length=50)
@@ -21,7 +24,7 @@ class Notes(models.Model):
     text = models.TextField(max_length=500, verbose_name="Text")
     is_public = models.BooleanField(verbose_name="Is public")
     is_important = models.BooleanField(verbose_name="Is important")
-    finish_date = models.DateTimeField(default=datetime.now()+timedelta(days=1), verbose_name="End time")
+    finish_date = models.DateTimeField(default=note_time, verbose_name="End time")
     author = models.ForeignKey(Authors, on_delete=models.PROTECT, verbose_name="Author")
     state = models.ForeignKey(States, on_delete=models.PROTECT, verbose_name="State")
 
